c9f1f588a90dd399a9c8011ea3291f424dec4257
Mayank Pal <mayankpal9654@gmail.com>
2025-05-16 16:12:05 +0530

    UC 8: Add Home component with react-router-dom routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,15 @@
-import { useState } from "react";
-import "./App.css";
-import LogoComponents from "./components/LogoComponents";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import './index.css';
 
 function App() {
-  const [userName, setUserName] = useState("");
-  const [error, setError] = useState("");
-  const logoLink = "https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg ";
-  const siteUrl = "https://www.bridgelabz.com";
-
-  const handleChange = (e) => {
-    const value = e.target.value;
-    setUserName(value);
-
-    const isValid = /^[A-Z]/.test(value) && value.length >= 3;
-    setError(
-      isValid
-        ? ""
-        : "Name must start with a capital letter and be at least 3 characters long."
-    );
-  };
-
   return (
-
-      <div className="container">
-
-      
-      <h1 className="message">
-        {userName && !error? `Hello ${userName} from bridgelabz` : "Hello from bridgelabz"}
-      </h1>
-      <div>
-        <LogoComponents logoLink={logoLink} siteUrl={siteUrl}> </LogoComponents>
-      </div>
-      <input
-        type="text"
-        value={userName}
-        onChange={handleChange}
-        placeholder="Name"
-      />
-
-      {error ? <p className="error">{error}</p> : null}
-      </div>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
 
   );
 }
---
+++ src/Home.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import './app.css';
+import LogoComponents from "./components/LogoComponents";
+
+function Home() {
+  const [userName, setUserName] = useState("");
+  const [error, setError] = useState("");
+  const logoLink = "https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg ";
+  const siteUrl = "https://www.bridgelabz.com";
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setUserName(value);
+
+    const isValid = /^[A-Z]/.test(value) && value.length >= 3;
+    setError(
+      isValid
+        ? ""
+        : "Name must start with a capital letter and be at least 3 characters long."
+    );
+  };
+
+  return (
+
+      <div className="container">
+
+      
+      <h1 className="message">
+        {userName && !error? `Hello ${userName} from bridgelabz` : "Hello from bridgelabz"}
+      </h1>
+      <div>
+        <LogoComponents logoLink={logoLink} siteUrl={siteUrl}> </LogoComponents>
+      </div>
+      <input
+        type="text"
+        value={userName}
+        onChange={handleChange}
+        placeholder="Name"
+      />
+
+      {error ? <p className="error">{error}</p> : null}
+      </div>
+
+  );
+}
+
+export default Home ;
