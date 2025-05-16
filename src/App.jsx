d968afd76c2744b79195995758aee57f4d013f7c
Mayank Pal <mayankpal9654@gmail.com>
2025-05-16 15:52:07 +0530

    ADD: CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,47 @@
-
-import { useState } from 'react';
-import './App.css'
-import LogoComponents from './components/LogoComponents';
+import { useState } from "react";
+import "./App.css";
+import LogoComponents from "./components/LogoComponents";
 
 function App() {
   const [userName, setUserName] = useState("");
   const [error, setError] = useState("");
-  const logoLink = 'https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg ';
-  const siteUrl = 'https://www.bridgelabz.com';
+  const logoLink = "https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg ";
+  const siteUrl = "https://www.bridgelabz.com";
 
-  const handleChange = (e)=> {
-      const value = e.target.value;
-      setUserName(value);
-      
-      const isValid = /^[A-Z]/.test(value) && value.length>=3;
-      setError(isValid?"":"Name must start with a capital letter and be at least 3 characters long.")
-    }
-  
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setUserName(value);
 
+    const isValid = /^[A-Z]/.test(value) && value.length >= 3;
+    setError(
+      isValid
+        ? ""
+        : "Name must start with a capital letter and be at least 3 characters long."
+    );
+  };
 
   return (
-    <>
-     <div><LogoComponents logoLink={logoLink} siteUrl={siteUrl} > </LogoComponents></div>
-     <input type="text" 
-      value={userName}
-      onChange={handleChange}
-      placeholder='Name'
-     />
-    
-    <h1>
-      {(userName && !error) ? `Hello ${userName} from bridgelabz` : "Hello from bridgelabz"}
-    </h1>
 
-    {error ? <p className="error">{error}</p> : null}
-    
+      <div className="container">
 
-    </>
-  )
+      
+      <h1 className="message">
+        {userName && !error? `Hello ${userName} from bridgelabz` : "Hello from bridgelabz"}
+      </h1>
+      <div>
+        <LogoComponents logoLink={logoLink} siteUrl={siteUrl}> </LogoComponents>
+      </div>
+      <input
+        type="text"
+        value={userName}
+        onChange={handleChange}
+        placeholder="Name"
+      />
+
+      {error ? <p className="error">{error}</p> : null}
+      </div>
+
+  );
 }
 
-export default App
-
+export default App;
