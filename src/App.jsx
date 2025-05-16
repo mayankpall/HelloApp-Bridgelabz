3f685d66a0784a40af4e4a64c517f3e687bde3a6
Mayank Pal <mayankpal9654@gmail.com>
2025-05-16 14:12:56 +0530

    UC 5: Add user input for dynamic hello message

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 
+import { useState } from 'react';
 import './App.css'
 import LogoComponents from './components/LogoComponents';
 
 function App() {
-  const name = "Bridglabz";
+  const [userName, setUserName] = useState();
   const logoLink = 'https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg ';
   const siteUrl = 'https://www.bridgelabz.com';
+
+
   return (
     <>
-     <LogoComponents logoLink={logoLink} siteUrl={siteUrl} > </LogoComponents>
-     <div>Hello from {name}!  </div>
+     <div><LogoComponents logoLink={logoLink} siteUrl={siteUrl} > </LogoComponents></div>
+     <input type="text" 
+      value={userName}
+      onChange={(e)=> setUserName(e.target.value)}
+      placeholder='Message'
+     />
+    <h1>
+      {userName? `Hello ${userName} from bridgelabz` : "Hello from bridgelabz"}
+    </h1>
+
     </>
   )
 }
