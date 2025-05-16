4c33bfaf412201bb145507c4e73a30672d5635c7
Mayank Pal <mayankpal9654@gmail.com>
2025-05-16 12:05:43 +0530

    UC 3: Display BridgeLabz logo using property binding

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import './App.css'
 
 function App() {
   const name = "Bridglabz";
+  const logoLink = "https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg ";
 
   return (
     <>
+    <img src={logoLink} alt="BridgeLabz" />
      <div>Hello from {name}!  </div>
     </>
   )
